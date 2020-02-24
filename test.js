d6d842e3178cd855330998dbfc20686b97cdf8aa
TVThieu0402 <42349527+TVThieu0402@users.noreply.github.com>
2020-02-24 13:08:40 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 <script src="/Users/tuyen.thieu/Documents/GitHub/tvthieu0402.github.io/KalturaFullClient.min.js"></script>
 
 <script>
